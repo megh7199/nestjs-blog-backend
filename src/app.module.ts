c9347615e108052826aaadfcfc3d07acb39a009e
megh7199 <meghshah7199@gmail.com>
2022-03-23 12:13:27 +0530

    db connected

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {ConfigModule} from '@nestjs/config'
+import {TypeOrmModule} from '@nestjs/typeorm'
+
+require('dotenv').config();
 
 @Module({
-  imports: [],
+  imports: [ ConfigModule.forRoot({isGlobal:true}),
+    TypeOrmModule.forRoot({
+      type:'postgres',
+      synchronize:true,
+      autoLoadEntities:true,
+      url: 'postgres://vfrisdgx:bPljy1eQJNbNYmhokjnbOhPBKgRFgwjn@satao.db.elephantsql.com/vfrisdgx'
+    })
+  
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
